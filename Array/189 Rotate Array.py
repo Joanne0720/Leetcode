3a565f91ed9e24a7537160a7c02e09b11f6feabf
wuq720 <48305620+wuq720@users.noreply.github.com>
2019-09-19 08:42:54 +0800

    Update 189 Rotate Array.py

--- Array/189 Rotate Array.py
+++ Array/189 Rotate Array.py
@@ -1,8 +1,20 @@
+#class Solution:
+#    def rotate(self, nums: List[int], k: int) -> None:
+#        """
+#        Do not return anything, modify nums in-place instead.
+#        """
+#        n = len(nums)
+#        k %= n
+#        nums[:] = nums[-k:] + nums[:-k]
+
+# 方法二：反转三次
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
-        Do not return anything, modify nums in-place instead.
-        """
+            Do not return anything, modify nums in-place instead.
+            """
         n = len(nums)
         k %= n
-        nums[:] = nums[-k:] + nums[:-k]
+        nums[:] = nums[::-1]
+        nums[:k] = nums[:k][::-1]
+        nums[k:] = nums[k:][::-1]
