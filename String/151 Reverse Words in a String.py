58404dfd7ce0f8e5694aae97b68615f4f0ab4593
wuq720 <48305620+wuq720@users.noreply.github.com>
2019-09-23 22:00:17 +0800

    Create 151 Reverse Words in a String.py
    翻转法

---
+++ String/151 Reverse Words in a String.py
@@ -0,0 +1,54 @@
+# 翻转法
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = list(s)
+        n = len(s)
+
+        # 翻转数组
+        def reverse(s, i, j):
+            while i < j:
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+
+        # 翻转单个单词
+        def word_reverse(s):
+            # 用双指针找到一个单词
+            i = 0
+            j = 0
+            while i < n:
+                # 找到一个单词首字母
+                while i < n and s[i] == " ":
+                    i += 1
+                j = i
+                # 找到一个单词末位置
+                while j < n and s[j] != " ":
+                    j += 1
+                reverse(s, i, j - 1)
+                i = j
+
+        # 清除多余空格
+        def clean_space(s):
+            i = 0
+            j = 0
+            while j < n:
+                # 找到一个单词
+                while j < n and s[j] == " ":
+                    j += 1
+                # 单词朝前移
+                while j < n and s[j] != " ":
+                    s[i] = s[j]
+                    i += 1
+                    j += 1
+                # 移动下一个单词
+                while j < n and s[j] == " ":
+                    j += 1
+                if j < n:
+                    s[i] = " "
+                    i += 1
+            return "".join(s[:i])
+
+        reverse(s, 0, n - 1)
+        word_reverse(s)
+        return clean_space(s)
