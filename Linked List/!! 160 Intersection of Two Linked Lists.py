f6a6f8021af621b1832a74ec1a9a5198be2decd3
wuq720 <48305620+wuq720@users.noreply.github.com>
2019-10-02 19:53:12 +0800

    Create !! 160 Intersection of Two Linked Lists.py
    消除长度差的两个方法：1.长链表指针先走 2.拼接两链表√

---
+++ Linked List/!! 160 Intersection of Two Linked Lists.py
@@ -0,0 +1,22 @@
+"""
+重要思路：消除长度差（如果两个速度相同的指针同时到达终点，最后一段路一定是一起走）
+消除长度差的两个方法：1.长链表指针先走 2.拼接两链表√
+
+指针 pA 指向 A 链表，指针 pB 指向 B 链表，依次往后遍历
+如果 pA 到了末尾，则 pA = headB 继续遍历
+如果 pB 到了末尾，则 pB = headA 继续遍历
+若在某一时刻 pA和pB相遇，则pA/pB为相交结点。
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
