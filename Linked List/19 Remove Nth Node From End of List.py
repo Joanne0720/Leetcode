d2f5d368ecf29297d88a9fe7450c5c8bc66463ee
wuq720 <48305620+wuq720@users.noreply.github.com>
2019-10-04 10:25:40 +0800

    双指针，一次遍历

--- Linked List/19 Remove Nth Node From End of List.py
+++ Linked List/19 Remove Nth Node From End of List.py
@@ -11,3 +11,15 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
+        fast, slow = head, head
+        # 找到要删除的节点的前面一个节点
+        for _ in range(n):
+            fast = fast.next
+        # 可能一共只有n个节点，即删除头结点
+        if fast is None:
+            head = head.next
+            return head
+        while fast.next:
+            fast, slow = fast.next, slow.next
+        slow.next = slow.next.next
+        return head
