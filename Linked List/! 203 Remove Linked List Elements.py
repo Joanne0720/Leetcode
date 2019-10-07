d03a45a93301100679d9d61583ff03034dda0810
wuq720 <48305620+wuq720@users.noreply.github.com>
2019-10-07 10:51:12 +0800

    用递归删除链表元素

---
+++ Linked List/! 203 Remove Linked List Elements.py
@@ -0,0 +1,18 @@
+"""
+法1：找到第一个不为val的结点作为头结点,定义两个指针进行删除操作;
+法2：定义一个新链表指向head结点,进行删除操作;
+法3：递归的从后往前进行删除操作;
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if not head:
+            return
+        head.next = self.removeElements(head.next, val)
+        return head.next if head.val == val else head
