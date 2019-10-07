e63cf1707cc1d9110576501caa76a24735cb796d
wuq720 <48305620+wuq720@users.noreply.github.com>
2019-10-07 10:53:05 +0800

    用递归删除链表元素

--- Linked List/! 203 Remove Linked List Elements.py
+++ Linked List/! 203 Remove Linked List Elements.py
@@ -13,6 +13,6 @@
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
         if not head:
-            return
+            return None
         head.next = self.removeElements(head.next, val)
         return head.next if head.val == val else head
