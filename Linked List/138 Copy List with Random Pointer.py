e55a8faa3c7d99247f5f56fe3bcbe914a4709f2f
wuq720 <48305620+wuq720@users.noreply.github.com>
2019-10-19 15:06:06 +0800

    方法二：旧节点和新节点交错的方法，不需要额外空间

--- Linked List/138 Copy List with Random Pointer.py
+++ Linked List/138 Copy List with Random Pointer.py
@@ -28,3 +28,30 @@ class Solution:
             return clone
 
         return DFS(head)
+
+
+
+# 方法二：旧节点和新节点交错的方法，不需要额外空间
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if not head:
+            return head
+        cur = head
+        # Cloned node,weaving, A->B->C: A->A'->B->B'->C->C'
+        while cur:
+            clone = Node(cur.val)
+            clone.next, cur.next = cur.next, clone
+            cur = clone.next
+        cur = head
+        # link the random pointers
+        while cur:
+            cur.next.random = cur.random.next if cur.random else None
+            cur = cur.next.next
+        # Unweave the linked list, A->A'->B->B'->C->C': A->B->C and A'->B'->C'
+        newHead = head.next
+        old, new = head, head.next
+        while old:
+            old.next = old.next.next
+            new.next = new.next.next if new.next else None
+            old, new = old.next, new.next
+        return newHead
