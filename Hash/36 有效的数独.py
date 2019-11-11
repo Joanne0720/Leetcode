feac730ed180c8096ea427fe80adfa3e5950d3e6
wuq720 <48305620+wuq720@users.noreply.github.com>
2019-11-11 18:13:05 +0800

    数独

--- Hash/36 有效的数独.py
+++ Hash/36 有效的数独.py
@@ -1,2 +1,10 @@
+# 在一次迭代中完成对行、列、子数独的检查
+
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [{} for _ in range(9)]
+        columns = [{} for _ in range(9)]
+        boxes = [{} for _ in range(9)]
+        for i in range(9):
+            for j in range(9):
