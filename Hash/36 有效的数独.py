8fa5d44d255263111fabd17dc5c982749178b9e9
wuq720 <48305620+wuq720@users.noreply.github.com>
2019-11-12 08:11:02 +0800

    数独

--- Hash/36 有效的数独.py
+++ Hash/36 有效的数独.py
@@ -8,3 +8,14 @@ class Solution:
         boxes = [{} for _ in range(9)]
         for i in range(9):
             for j in range(9):
+                num = board[i][j]
+                if num != '.':
+                    num = int(num)
+
+                    rows[i][num] = rows[i].get(num, 0) + 1
+                    columns[j][num] = columns[j].get(num, 0) + 1
+                    boxes[(i // 3) * 3 + j // 3][num] = boxes[(i // 3) * 3 + j // 3].get(num, 0) + 1
+
+                    if rows[i][num] > 1 or columns[j][num] > 1 or boxes[(i // 3) * 3 + j // 3][num] > 1:
+                        return False
+        return True
