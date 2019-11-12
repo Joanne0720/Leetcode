71127d11c7e6975e5a43103b7dafd691b5b03bee
wuq720 <48305620+wuq720@users.noreply.github.com>
2019-11-12 08:13:19 +0800

    数独

--- Hash/36 有效的数独.py
+++ Hash/36 有效的数独.py
@@ -11,11 +11,11 @@ class Solution:
                 num = board[i][j]
                 if num != '.':
                     num = int(num)
-
+                    # 字典(Dictionary) get() 函数返回指定键的值，如果值不在字典中返回默认值。dict.get(key, default=None)
                     rows[i][num] = rows[i].get(num, 0) + 1
                     columns[j][num] = columns[j].get(num, 0) + 1
                     boxes[(i // 3) * 3 + j // 3][num] = boxes[(i // 3) * 3 + j // 3].get(num, 0) + 1
-
+                    # 加入字典后，判断是否有效
                     if rows[i][num] > 1 or columns[j][num] > 1 or boxes[(i // 3) * 3 + j // 3][num] > 1:
                         return False
         return True
