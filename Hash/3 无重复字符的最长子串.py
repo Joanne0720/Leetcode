aa443f2c51b027d673f1a573cae70e8e7f30c9bc
wuq720 <48305620+wuq720@users.noreply.github.com>
2019-11-16 22:12:11 +0800

    ...

--- Hash/3 无重复字符的最长子串.py
+++ Hash/3 无重复字符的最长子串.py
@@ -1,2 +1,14 @@
+# 滑动窗口，从字典里找下一个窗口的start
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        d = {}
+        start = -1
+        r = 0
+        for i, c in s:
+            if c in d:
+                d[c] = i
+            else:
+                d[c] = i
+                r += 1
