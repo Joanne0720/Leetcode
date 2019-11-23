292ec60623dc8dc5fd79cd1fc8dc2f006de1e274
wuq720 <48305620+wuq720@users.noreply.github.com>
2019-11-23 11:42:10 +0800

    把四个数组分为两组

--- Hash/454 四数相加 II.py
+++ Hash/454 四数相加 II.py
@@ -1,2 +1,7 @@
+from collections import Counter
+
+
 class Solution:
     def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        h = Counter(a + b for a in A for b in B)
+        return sum(h[-c-d] for c in C for d in D)
